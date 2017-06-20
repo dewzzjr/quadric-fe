75f434d8f588537a550695041832f0ba12522941
akmalfaza <akmalfaza17@gmail.com>
2017-06-20 10:42:14 +0700

    commit model

--- application/models/model_FE.php
+++ application/models/model_FE.php
@@ -8,169 +8,148 @@
 	    }
 
 		function YTD($tahun){
+			$cond = array('YEAR(SC_TGLPS)' => $tahun);
+
 			$YTD = $this->db->select('WITEL, AVG(SLG="COMPLY") AVG_YTD')
 								->from('reg_data')
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
 			$YTD_YKT = $this->db->select('AVG(SLG="COMPLY") AVG_YTD_YKT')
 								->from('reg_data')
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->like('WITEL','YOGYAKARTA')
 								->get();
 
 			$YTD_total = $this->db->select('AVG(SLG="COMPLY") AVG_YTD_total')
 								->from('reg_data')
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->get();
 			echo '<pre>';var_dump($YTD_YKT->result());
 
 		}
 
 		function MTD($bulan, $tahun){
+			$cond = array('YEAR(SC_TGLPS)' => $tahun, 'MONTH(SC_TGLPS)' => $bulan);
+
 			$MTD = $this->db->select('WITEL, AVG(SLG="COMPLY") AVG_MTD')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
 			$MTD_YKT = $this->db->select('AVG(SLG="COMPLY") AVG_MTD_YKT')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->like('WITEL','YOGYAKARTA')
 								->get();
 
 			$MTD_total = $this->db->select('AVG(SLG="COMPLY") AVG_MTD_total')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
+								->where($cond)
 								->get();
 			echo '<pre>';var_dump($MTD->result());
 		}
 
 		function DTD($tanggal){
+			$cond = array('SC_TGLPS' => $tanggal);
+
 			$DTD = $this->db->select('WITEL, AVG(SLG="COMPLY") AVG_DTD')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
 			$DTD_YKT = $this->db->select('AVG(SLG="COMPLY") AVG_DTD_YKT')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
+								->where($cond)
 								->like('WITEL','YOGYAKARTA')
 								->get();
 
 			$DTD_total = $this->db->select('AVG(SLG="COMPLY") AVG_DTD_total')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
+								->where($cond)
 								->get();
 			echo '<pre>';var_dump($DTD->result());
 		}
 
 		function REPORT_PS_DTD($tanggal){
+			$cond = array('SC_TGLPS' => $tanggal);
+
 			$PS_3 = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
 			$PS_3_YKT = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->like('WITEL','YOGYAKARTA')
-								->group_by('WITEL')
 								->get();
 
 			$PS_3_total = $this->db->select('SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->get();
 
-			$PS_2 = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2 = $this->db->select('WITEL, SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
-			$PS_2_YKT = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2_YKT = $this->db->select('SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '1 HARI')
-								->like('WITEL','YOGYAKARTA')
-								->group_by('WITEL')
+								->where($cond)
 								->get();
 
-			$PS_2_total = $this->db->select('SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2_total = $this->db->select('SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('SC_TGLPS', $tanggal)
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->get();
-			echo '<pre>';var_dump($PS_3->result());
+			echo '<pre>';var_dump($PS_2->result());
 		}
 
 		function REPORT_PS_MTD($bulan, $tahun){
+			$cond = array('YEAR(SC_TGLPS)' => $tahun, 'MONTH(SC_TGLPS)' => $bulan);
+
 			$PS_3 = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
 			$PS_3_YKT = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
 								->like('WITEL','YOGYAKARTA')
-								->group_by('WITEL')
+								->where($cond)
 								->get();
 
 			$PS_3_total = $this->db->select('SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '2 HARI')
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->get();
 
-			$PS_2 = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2 = $this->db->select('WITEL, SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->group_by('WITEL')
 								->get();
 
-			$PS_2_YKT = $this->db->select('WITEL, SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2_YKT = $this->db->select('SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->like('WITEL','YOGYAKARTA')
-								->group_by('WITEL')
 								->get();
 
-			$PS_2_total = $this->db->select('SUM(SLG="COMPLY") CNT_COMPLY, SUM(SLG="NOT COMPLY") CNT_NOTCOMPLY, COUNT(SLG)')
+			$PS_2_total = $this->db->select('SUM(DURASI <= 2) CNT_COMPLY, SUM(DURASI >= 3) CNT_NOTCOMPLY, COUNT(DURASI)')
 								->from('reg_data')
-								->where('MONTH(SC_TGLPS)', $bulan)
-								->where('YEAR(SC_TGLPS)', $tahun)
-								->where('MTTI_GROUP', '1 HARI')
+								->where($cond)
 								->get();
-			echo '<pre>';var_dump($PS_3->result());
+		
+			echo '<pre>';var_dump($PS_2_YKT->result());
 		}
 
 	}
