911a56130d0ec4843b01e2dd840568e5e2643b04
akmalfaza <akmalfaza17@gmail.com>
2017-07-03 08:28:10 +0700

    add input

--- application/models/model_FE.php
+++ application/models/model_FE.php
@@ -7,6 +7,30 @@
 	        $this->load->database();
 	    }
 
+		function importData(){
+			$file = fopen($_FILES['userfile']['tmp_name'],'r') or die("can't open file");
+
+			return $query = $this->db->query(
+			"LOAD DATA INFILE '<?php $file ?>' 
+			INTO TABLE reg_data 
+			FIELDS TERMINATED BY '\t' 
+			LINES TERMINATED BY '\n'
+			IGNORE 1 ROWS
+			(ALPRO, BEFORE_CA, CHANEL, CHANEL_APP, CSEG, Cancel, MTTI_GROUP, `MTD MTTI`, SLG, APP, DURASI, DURASI_FO, DURASI_PI, DURASI_RE, DURASI_UN, DURASI_VA, @var1, JENISPSB, KANDATEL, KAWASAN, KODEFIKASI_SC, NDEM, `Number of Records`, ORDER_ID, PS_DONE, REASON_CANCEL, @var2, @var3, @var4, @var5, @var6, @var7, `SLG (copy)`, STATUS, STATUS_INDIHOME, STATUS_SERVICE, TYPE_PIPE, WITEL)
+			SET ISISKA_TGLVA = STR_TO_DATE(@var1, '%m/%d/%Y %r'), SC_TGLCA = STR_TO_DATE(@var2, '%m/%d/%Y %r'), SC_TGLCREATE = STR_TO_DATE(@var3, '%m/%d/%Y %r'), SC_TGLFO = STR_TO_DATE(@var4, '%m/%d/%Y %r'), SC_TGLPI = STR_TO_DATE(@var5, '%m/%d/%Y %r'), SC_TGLPS = STR_TO_DATE(@var6, '%m/%d/%Y %r'), SC_TGLUNSC = STR_TO_DATE(@var7, '%m/%d/%Y %r')");
+		}
+
+		function listTanggal(){
+			$this->db->distinct();
+			$this->db->select('SC_TGLPS');
+			$listTanggal = $this->db->get('reg_data');
+		}
+
+		function deleteData($tanggal){
+			$this->db->where('SC_TGLPS', $tanggal);
+			$this->db->delete('reg_data'); 
+		}
+
 		function YTD($tahun){
 			$cond = array('YEAR(SC_TGLPS)' => $tahun);
 
@@ -14,19 +38,24 @@
 								->from('reg_data')
 								->where($cond)
 								->group_by('WITEL')
-								->get();
+								->get()
+								->result();
 
 			$YTD_YKT = $this->db->select('AVG(SLG="COMPLY") AVG_YTD_YKT')
 								->from('reg_data')
 								->where($cond)
 								->like('WITEL','YOGYAKARTA')
-								->get();
+								->get()
+								->result();
 
 			$YTD_total = $this->db->select('AVG(SLG="COMPLY") AVG_YTD_total')
 								->from('reg_data')
 								->where($cond)
-								->get();
-			echo '<pre>';var_dump($YTD_YKT->result());
+								->get()
+								->result();
+
+			return array('YTD' => $YTD, 'YTD_total' => $YTD_total);
+			//echo '<pre>';var_dump($YTD_YKT->result());
 
 		}
 
@@ -37,19 +66,24 @@
 								->from('reg_data')
 								->where($cond)
 								->group_by('WITEL')
-								->get();
+								->get()
+								->result();
 
 			$MTD_YKT = $this->db->select('AVG(SLG="COMPLY") AVG_MTD_YKT')
 								->from('reg_data')
 								->where($cond)
 								->like('WITEL','YOGYAKARTA')
-								->get();
+								->get()
+								->result();
 
 			$MTD_total = $this->db->select('AVG(SLG="COMPLY") AVG_MTD_total')
 								->from('reg_data')
 								->where($cond)
-								->get();
-			echo '<pre>';var_dump($MTD->result());
+								->get()
+								->result();
+
+			return array('MTD' => $MTD, 'MTD_total' => $MTD_total);
+			//echo '<pre>';var_dump($MTD->result());
 		}
 
 		function DTD($tanggal){
@@ -151,5 +185,7 @@
 		
 			echo '<pre>';var_dump($PS_2_YKT->result());
 		}
+
+		
 
 	}
