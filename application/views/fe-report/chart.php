c392fd9df8b41c376b022287e8490abcdc9bdf17
Dewangga P P <dewzzpro@gmail.com>
2017-08-03 12:00:34 +0700

    add chart

---
+++ application/views/fe-report/chart.php
@@ -0,0 +1,18 @@
+<div class="box box-info">
+  <div class="box-header with-border">
+    <h3 class="box-title">{title}</span></h3>
+
+    <div class="box-tools pull-right">
+      <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+      </button>
+      <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+    </div>
+  </div>
+  <div class="box-body">
+    <div class="chart">
+      <canvas id="{tipe}-chart" style="height:{size}px"></canvas>
+    </div>
+  </div>
+  <!-- /.box-body -->
+</div>
+<!-- /.box -->
